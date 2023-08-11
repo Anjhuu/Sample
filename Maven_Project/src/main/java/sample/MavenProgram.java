2227c0f293a7ce32ddf592228bd8435d937075c5
USER <USER@LAPTOP-IPA4K501>
2023-08-11 10:32:04 +0530

    Hello world

---
+++ Maven_Project/src/main/java/sample/MavenProgram.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class MavenProgram {
+	public void hai()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+	MavenProgram a= new MavenProgram();
+	a.hai();
+	
+
+	}
+
+}
